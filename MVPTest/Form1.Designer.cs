2c262d0e91bc64b3cea73f45d66e717a81a85b9f
OSandh <oskarsandh@gmail.com>
2018-02-06 19:58:09 +0100

    TestChange

--- MVPTest/Form1.Designer.cs
+++ MVPTest/Form1.Designer.cs
@@ -47,7 +47,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "GoToView";
+            this.button1.Text = "GoToVieW";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
